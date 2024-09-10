c5ba580947363794a43c8721128c3c53b54b380d
Maryeality <mm.vering@icloud.com>
2024-09-10 22:19:18 +0200

    erste buttons mit toggle funktion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"maryamtrinken","appId":"1:88302657392:web:27052f894e03e8669b39b5","storageBucket":"maryamtrinken.appspot.com","apiKey":"AIzaSyDOBiLTv2uEQ75eYOyIaoT23LzTODTX3-A","authDomain":"maryamtrinken.firebaseapp.com","messagingSenderId":"88302657392"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"maryamtrinken","appId":"1:88302657392:web:27052f894e03e8669b39b5","storageBucket":"maryamtrinken.appspot.com","apiKey":"AIzaSyDOBiLTv2uEQ75eYOyIaoT23LzTODTX3-A","authDomain":"maryamtrinken.firebaseapp.com","messagingSenderId":"88302657392"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
 };
--- src/app/gamemodeselection/gamemodeselection.component.ts
+++ src/app/gamemodeselection/gamemodeselection.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gamemodeselection',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, CommonModule],
   templateUrl: './gamemodeselection.component.html',
   styleUrl: './gamemodeselection.component.scss',
 })
 export class GamemodeselectionComponent {
-  title: string = '';
+  isSanftButtonDisabled = true;
+  isMittelButtonDisabled = true;
+  isHartButtonDisabled = true;
+
+  toggleButtonSanft() {
+    this.isSanftButtonDisabled = !this.isSanftButtonDisabled;
+  }
+  toggleButtonMittel() {
+    this.isMittelButtonDisabled = !this.isMittelButtonDisabled;
+  }
+
+  toggleButtonHart() {
+    this.isHartButtonDisabled = !this.isHartButtonDisabled;
+  }
 }
